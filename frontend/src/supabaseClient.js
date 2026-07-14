130f1097bc5d1325b4a2b4a9192f1905515db0b1
mili sheta <mili.we3vision@gmail.com>
2026-07-14 14:20:02 +0530

    Delete frontend directory

--- frontend/src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// frontend/src/supabaseClient.js- Drashti
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
